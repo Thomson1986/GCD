54823d301f39ba9ea6e2406e39fafb2c0504de4e
Thomson Varghese <thomsonmca@gmail.com>
2024-05-05 08:59:29 -0500

    GCD initial

---
+++ DispatchQueue.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+//import PlaygroundSupport
+
+//PlaygroundPage.current.needsIndefiniteExecution = true
+
+let workItem = DispatchWorkItem {
+    print(Thread.isMainThread ? 
+          print("Executing in main thread") :
+            print("Executing in other thread"))
+}
+
+DispatchQueue.main.async(execute: workItem)
+
+DispatchQueue.global().async {
+    print(Thread.isMainThread ?
+          print("Executing in main thread") :
+            print("Executing in background thread"))
+}
